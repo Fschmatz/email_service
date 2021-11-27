7967d25c9239b4542b9ae62c2635716e94194066
Fschmatz <fschmatz@hotmail.com>
2021-11-27 17:10:31 -0300

    v 0.2.0

--- src/main/java/com/fschmatz/email_service/controller/EmailController.java
+++ src/main/java/com/fschmatz/email_service/controller/EmailController.java
@@ -3,6 +3,7 @@ package com.fschmatz.email_service.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +16,41 @@ public class EmailController {
     @Autowired
     private JavaMailSender mailSender;
 
-    @RequestMapping(path = "/cancelamento", method = RequestMethod.GET)
-    public String sendMailCancelamento() {
+
+    //UM EMAIL PRA TER UM CHAMADO UNIVERSAL
+    @RequestMapping(path = "/send/{nome}/{msg}", method = RequestMethod.GET)
+    public String sendMail(@PathVariable("nome") String nomeUsuario,@PathVariable("msg") String mensagem) {
+
+        System.out.println("Disparou !!!!!");
+
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setText("Você cancelou");
+        message.setText(mensagem+"\n "+nomeUsuario);
+        message.setTo("tvsalastickmi@gmail.com");
+        message.setFrom("tvsalastickmi@gmail.com");
+
+        try {
+            mailSender.send(message);
+            return "Email enviado com sucesso!";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Erro ao enviar email.";
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    @RequestMapping(path = "/cancelamento/{nome}", method = RequestMethod.GET)
+    public String sendMailCancelamento(@PathVariable("nome") String nomeUsuario) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setText("Você cancelou, "+nomeUsuario);
         message.setTo("tvsalastickmi@gmail.com");
         message.setFrom("tvsalastickmi@gmail.com");
 
@@ -32,9 +64,9 @@ public class EmailController {
     }
 
     @RequestMapping(path = "/inscricao/{nome}", method = RequestMethod.GET)
-    public String sendMailInscricao() {
+    public String sendMailInscricao(@PathVariable("nome") String nomeUsuario) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setText("Você se inscreveu");
+        message.setText("Você se inscreveu "+nomeUsuario);
         message.setTo("tvsalastickmi@gmail.com");
         message.setFrom("tvsalastickmi@gmail.com");
 
@@ -48,9 +80,9 @@ public class EmailController {
     }
 
     @RequestMapping(path = "/certificado/{nome}", method = RequestMethod.GET)
-    public String sendMailCertificado() {
+    public String sendMailCertificado(@PathVariable("nome") String nomeUsuario) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setText("Seu certificado");
+        message.setText("Seu certificado "+nomeUsuario);
         message.setTo("tvsalastickmi@gmail.com");
         message.setFrom("tvsalastickmi@gmail.com");
 
